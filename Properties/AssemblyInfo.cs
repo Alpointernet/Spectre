e076ab38ce9d2572ceb94f10df042093172faabc
Alpointernet <alpointernet@example.com>
2026-08-11 22:41:32 +0300

    bumped version to 1.0.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,3 +2,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: System.Runtime.Versioning.SupportedOSPlatform("windows10.0.19041.0")]
+[assembly: System.Reflection.AssemblyVersion("1.0.1.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.1.0")]
